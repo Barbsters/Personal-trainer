e1968103177288a5739c04c3a9f09ce75df0b77a
Barbara Jerko <47761051+Barbsters@users.noreply.github.com>
2020-04-09 23:24:50 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
+import Customerlist from './components/Customerlist';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <AppBar position="static" style={{ background: '#F08080'}}>
+        <Toolbar>
+          <Typography variant="h6">
+            Barbster Personal Trainer
+          </Typography>
+        </Toolbar>
+      </AppBar>
 
-      </header>
+      <Customerlist />
     </div>
   );
 }
+
 
 export default App;
